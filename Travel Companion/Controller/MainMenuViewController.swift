94ea3b674e05eb8d1874ed129388f47106ecf7cf
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-08-02 23:17:38 +0200

    feat: Add MainMenu

---
+++ Travel Companion/Controller/MainMenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainMenuViewController.swift
+//  
+//
+//  Created by Stefan Jaindl on 02.08.18.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
