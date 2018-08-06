4da8c0a339b7b76f728aa017588e27e2cf99bea8
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-08-06 21:09:39 +0200

    feat: Implement MainMenu with authentication

--- Travel Companion/Controller/MainMenuViewController.swift
+++ Travel Companion/Controller/MainMenuViewController.swift
@@ -6,13 +6,65 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuthUI
+import FirebaseFacebookAuthUI
+import FirebaseGoogleAuthUI
 
 class MainMenuViewController: UIViewController {
 
+    fileprivate var _authHandle: AuthStateDidChangeListenerHandle!
+    var user: User?
+    var displayName = "Anonymous"
+    
+    @IBOutlet weak var exploreImage: UIImageView!
+    @IBOutlet weak var planImage: UIImageView!
+    @IBOutlet weak var rememberImage: UIImageView!
+    
+    @IBOutlet weak var exploreLabel: UILabel!
+    @IBOutlet weak var planLabel: UILabel!
+    @IBOutlet weak var rememberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureAuth()
         // Do any additional setup after loading the view.
+        configureGestureRecognizers()
+    }
+    
+    func configureGestureRecognizers() {
+        addGestureRecognizer(selector: #selector(explore), view: exploreImage)
+        addGestureRecognizer(selector: #selector(explore), view: exploreLabel)
+        addGestureRecognizer(selector: #selector(plan), view: planImage)
+        addGestureRecognizer(selector: #selector(plan), view: planLabel)
+        addGestureRecognizer(selector: #selector(remember), view: rememberImage)
+        addGestureRecognizer(selector: #selector(remember), view: rememberLabel)
+    }
+    
+    func addGestureRecognizer(selector: Selector?, view: UIView) {
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: selector)
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc
+    func explore() {
+        print("explore")
+    }
+    
+    @objc
+    func plan() {
+        print("plan")
+    }
+    
+    @objc
+    func remember() {
+        print("remember")
+    }
+    
+    deinit {
+        Auth.auth().removeStateDidChangeListener(_authHandle)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,7 +72,13 @@ class MainMenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func signOut(_ sender: Any) {
+        user = nil
+        try? Auth.auth().signOut()
+        
+        loginSession()
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -30,5 +88,37 @@ class MainMenuViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func configureAuth() {
+        let provider: [FUIAuthProvider] = [FUIGoogleAuth(), FUIFacebookAuth()] 
+        FUIAuth.defaultAuthUI()?.providers = provider
+        
+        // listen for changes in the authorization state
+        _authHandle = Auth.auth().addStateDidChangeListener { (auth: Auth, user: User?) in
+            // refresh table data
+//            self.messages.removeAll(keepingCapacity: false)
+//            self.messagesTable.reloadData()
+            
+            // check if there is a current user
+            if let activeUser = user {
+                // check if the current app user is the current FIRUser
+                if self.user != activeUser {
+                    self.user = activeUser
+//                    self.signedInStatus(isSignedIn: true)
+                    let name = user!.email!.components(separatedBy: "@")[0]
+                    self.displayName = name
+                }
+            } else {
+                // user must sign in
+//                self.signedInStatus(isSignedIn: false)
+                self.loginSession()
+            }
+        }
+    }
 
+    func loginSession() {
+        let authViewController = FUIAuth.defaultAuthUI()!.authViewController()
+        present(authViewController, animated: true, completion: nil)
+    }
+    
 }
