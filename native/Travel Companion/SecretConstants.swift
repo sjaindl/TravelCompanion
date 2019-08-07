343aeb1b5037d443543c2cc5cc8aef667ffa224f
Stefan Jaindl <jaindl.stefan@gmail.com>
2019-08-08 01:40:57 +0200

    feat: Move docs folder, Add SecretConstants file

---
+++ native/Travel Companion/SecretConstants.swift
@@ -0,0 +1,17 @@
+//
+//  SecretConstants.swift
+//  Travel Companion
+//
+//  Created by Stefan Jaindl on 01.08.18.
+//  Copyright © 2018 Stefan Jaindl. All rights reserved.
+//
+
+import Foundation
+
+class SecretConstants {
+    static let apiKeyGoogleMaps = "placeholder"
+    static let apiKeyGooglePlaces = "placeholder"
+    static let apiKeyFlickr = "placeholder"
+    static let apiKeyRomeToRio = "placeholder"
+    static let userNameGeoNames = "placeholder"
+}
