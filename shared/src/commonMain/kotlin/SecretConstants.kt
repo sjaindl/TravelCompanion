9999c5ee46d565eca521bb9b1a38ad289ebc7adc
Stefan Jaindl <jaindl.stefan@gmail.com>
2020-04-12 01:01:46 +0200

    feat: Add Kotlin files for Native support

---
+++ shared/src/commonMain/kotlin/SecretConstants.kt
@@ -0,0 +1,19 @@
+package com.sjaindl.travelcompanion
+
+//
+//  SecretConstants.swift
+//  Travel Companion
+//
+//  Created by Stefan Jaindl on 17.08.18.
+//  Copyright © 2018 Stefan Jaindl. All rights reserved.
+//
+
+data class SecretConstants (
+
+    var apiKeyGoogleMaps: String = "placeholder",
+    var apiKeyGooglePlaces: String = "placeholder",
+    var apiKeyFlickr: String = "placeholder",
+    var apiKeyRomeToRio: String = "placeholder",
+    var userNameGeoNames: String = "placeholder"
+
+)
