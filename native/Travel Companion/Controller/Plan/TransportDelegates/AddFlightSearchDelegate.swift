a8943e1bdec31ba5bde8270d3f9491696ec11bc0
Stefan Jaindl <jaindl.stefan@gmail.com>
2022-07-18 21:57:13 +0200

    Disable noCar param for Rome2Rio flight searches

--- native/Travel Companion/Controller/Plan/TransportDelegates/AddFlightSearchDelegate.swift
+++ native/Travel Companion/Controller/Plan/TransportDelegates/AddFlightSearchDelegate.swift
@@ -9,16 +9,16 @@
 import Foundation
 
 class AddFlightSearchDelegate: NSObject, AddTransportSearchDelegate {
-
     func buildSearchQueryItems(origin: String, destination: String) -> [String: String] {
-        return [
+        [
             Rome2RioConstants.ParameterKeys.key: SecretConstants.apiKeyRomeToRio,
             Rome2RioConstants.ParameterKeys.originName: origin,
             Rome2RioConstants.ParameterKeys.destinationName: destination,
             Rome2RioConstants.ParameterKeys.noRail: "true",
             Rome2RioConstants.ParameterKeys.noBus: "true",
             Rome2RioConstants.ParameterKeys.noFerry: "true",
-            Rome2RioConstants.ParameterKeys.noCar: "true",
+            // better to disable because there may be a car drive necessary to the next airport:
+            // Rome2RioConstants.ParameterKeys.noCar: "true",
             Rome2RioConstants.ParameterKeys.noBikeshare: "true",
             Rome2RioConstants.ParameterKeys.noRideshare: "true",
             Rome2RioConstants.ParameterKeys.noTowncar: "true",
