7536c6087488ca53cdaa4b8b6071f10ee00b85ec
Author Name <jaindl.stefan@gmail.com>
2023-04-02 14:40:35 +0200

    Inject items into DetailsTabBarLayoutPreview

--- app/src/main/java/com/sjaindl/travelcompanion/explore/details/DetailsTabBarLayout.kt
+++ app/src/main/java/com/sjaindl/travelcompanion/explore/details/DetailsTabBarLayout.kt
@@ -1,34 +1,36 @@
 package com.sjaindl.travelcompanion.explore.details
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Photo
+import androidx.compose.material.icons.rounded.Place
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.google.accompanist.pager.rememberPagerState
+import com.sjaindl.travelcompanion.R
 import com.sjaindl.travelcompanion.theme.TravelCompanionTheme
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun DetailsTabBarLayout() {
+fun DetailsTabBarLayout(tabRowItems: List<TabItem>) {
     val pagerState = rememberPagerState()
     val coroutineScope = rememberCoroutineScope()
 
     TravelCompanionTheme {
-        Column(
-            modifier = Modifier.padding(0.dp),
-            verticalArrangement = Arrangement.Bottom
-        ) {
+        Column {
             TabRow(
                 selectedTabIndex = pagerState.currentPage,
                 indicator = { tabPositions ->
@@ -41,7 +43,11 @@ fun DetailsTabBarLayout() {
                 tabRowItems.forEachIndexed { index, item ->
                     Tab(
                         selected = pagerState.currentPage == index,
-                        onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } },
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        },
                         icon = {
                             Icon(imageVector = item.icon, contentDescription = "")
                         },
@@ -55,9 +61,13 @@ fun DetailsTabBarLayout() {
                     )
                 }
             }
+
             HorizontalPager(
                 count = tabRowItems.size,
                 state = pagerState,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colors.background)
             ) {
                 tabRowItems[pagerState.currentPage].screen()
             }
@@ -68,5 +78,23 @@ fun DetailsTabBarLayout() {
 @Preview
 @Composable
 fun DetailsTabBarLayoutPreview() {
-    DetailsTabBarLayout()
+    DetailsTabBarLayout(
+        tabRowItems = listOf(
+            TabItem(
+                title = "Tab 1",
+                screen = { TabScreen(text = stringResource(id = R.string.detail)) },
+                icon = Icons.Rounded.Place,
+            ),
+            TabItem(
+                title = "Tab 2",
+                screen = { TabScreen(text = stringResource(id = R.string.photos)) },
+                icon = Icons.Rounded.Photo,
+            ),
+            TabItem(
+                title = "Tab 3",
+                screen = { TabScreen(text = stringResource(id = R.string.info)) },
+                icon = Icons.Rounded.Info,
+            )
+        )
+    )
 }
--- app/src/main/java/com/sjaindl/travelcompanion/explore/details/TabScreen.kt
+++ app/src/main/java/com/sjaindl/travelcompanion/explore/details/TabScreen.kt
@@ -17,7 +17,7 @@ import com.sjaindl.travelcompanion.R
 import com.sjaindl.travelcompanion.theme.TravelCompanionTheme
 
 @Composable
-private fun TabScreen(
+fun TabScreen(
     text: String,
 ) {
     TravelCompanionTheme {
@@ -32,24 +32,6 @@ private fun TabScreen(
         }
     }
 }
-
-val tabRowItems = listOf(
-    TabItem(
-        title = "Tab 1",
-        screen = { TabScreen(text = stringResource(id = R.string.detail)) },
-        icon = Icons.Rounded.Place,
-    ),
-    TabItem(
-        title = "Tab 2",
-        screen = { TabScreen(text = stringResource(id = R.string.photos)) },
-        icon = Icons.Rounded.Photo,
-    ),
-    TabItem(
-        title = "Tab 3",
-        screen = { TabScreen(text = stringResource(id = R.string.info)) },
-        icon = Icons.Rounded.Info,
-    )
-)
 
 @Preview
 @Composable
