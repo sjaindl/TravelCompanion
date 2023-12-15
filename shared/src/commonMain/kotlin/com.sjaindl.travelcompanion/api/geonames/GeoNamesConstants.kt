20bdaaf440bf042e233a40c2f33029e2566886f4
Stefan Jaindl <jaindl.stefan@gmail.com>
2023-12-15 12:03:01 +0100

    feat: const declarations

--- shared/src/commonMain/kotlin/com.sjaindl.travelcompanion/api/geonames/GeoNamesConstants.kt
+++ shared/src/commonMain/kotlin/com.sjaindl.travelcompanion/api/geonames/GeoNamesConstants.kt
@@ -2,17 +2,17 @@ package com.sjaindl.travelcompanion.api.geonames
 
 object GeoNamesConstants {
     object UrlComponents {
-        val urlProtocol: String = "https"
-        val domain: String = "secure.geonames.org"
-        val path: String = "/countryCode"
+        const val urlProtocol: String = "https"
+        const val domain: String = "secure.geonames.org"
+        const val path: String = "/countryCode"
 
-        val urlProtocolHttp: String = "http"
-        val domainHttp: String = "api.geonames.org"
+        const val urlProtocolHttp: String = "http"
+        const val domainHttp: String = "api.geonames.org"
     }
 
     object ParameterKeys {
-        val latitude: String = "lat"
-        val longitude: String = "lng"
-        val username: String = "username"
+        const val latitude: String = "lat"
+        const val longitude: String = "lng"
+        const val username: String = "username"
     }
 }
