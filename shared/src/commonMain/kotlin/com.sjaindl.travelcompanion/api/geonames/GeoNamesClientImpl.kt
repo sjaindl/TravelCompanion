cca7f88cceb068d438c4834d5a1f5d1fe90400f8
Stefan Jaindl <stefan.jaindl@neofonie.de>
2024-04-23 16:38:49 +0200

    fix(API): Prevent Geonames SSL certificate exception

--- shared/src/commonMain/kotlin/com.sjaindl.travelcompanion/api/geonames/GeoNamesClientImpl.kt
+++ shared/src/commonMain/kotlin/com.sjaindl.travelcompanion/api/geonames/GeoNamesClientImpl.kt
@@ -4,17 +4,16 @@ import com.sjaindl.travelcompanion.SecretConstants
 import com.sjaindl.travelcompanion.api.HttpResponseHandler
 import com.sjaindl.travelcompanion.api.logError
 import com.sjaindl.travelcompanion.util.Mockable
-import io.ktor.client.statement.*
-import io.ktor.http.*
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpMethod
 import kotlinx.serialization.SerializationException
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 @Mockable
 class GeoNamesClientImpl(private val httpResponseHandler: HttpResponseHandler) : GeoNamesClient {
     override suspend fun fetchCountryCode(latitude: Double, longitude: Double): String? {
         val urlComponents = GeoNamesConstants.UrlComponents
-        val baseUrl = "${urlComponents.urlProtocol}://${urlComponents.domain}/"
+        val baseUrl = "${urlComponents.urlProtocolHttp}://${urlComponents.domainHttp}"
 
         val response = httpResponseHandler.request(
             baseUrl = baseUrl,
