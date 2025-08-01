d48aaaf006316fd31bc83ce18d535e923b2ba86b
Author Name <jaindl.stefan@gmail.com>
2025-08-01 09:29:29 +0200

    Release: Android v5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.sjaindl.travelcompanion"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1"
+        versionCode = 5
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
